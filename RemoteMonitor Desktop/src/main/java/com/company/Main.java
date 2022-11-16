1c7fff391b89d6fcd4c815ff8e8065e8b3692685
omar <39051822+km@users.noreply.github.com>
2022-11-16 17:18:33 +0100

    Ram class

--- RemoteMonitor Desktop/src/main/java/com/company/Main.java
+++ RemoteMonitor Desktop/src/main/java/com/company/Main.java
@@ -10,13 +10,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CPU cpu = new CPU();
-        cpu.update();
-        System.out.println(cpu.getComponentName());
-        System.out.println(cpu.getCores());
-        System.out.println(cpu.getUsage());
-        System.out.println(cpu.getTemperature());
-        System.out.println(cpu.getCurrentSpeed()[1]);
-        System.out.println(cpu.getSpeed());
     }
 }
--- RemoteMonitor Desktop/src/main/java/components/Ram.java
+++ RemoteMonitor Desktop/src/main/java/components/Ram.java
@@ -1,6 +1,28 @@
 package components;
 
+import oshi.hardware.GlobalMemory;
+
 public class Ram extends Component{
-    private double memoryTotal;
-    private double memoryAvailable;
+    private long memoryTotal;
+    private long memoryAvailable;
+    private GlobalMemory gm;
+    public Ram()
+    {
+        gm = si.getHardware().getMemory();
+        memoryTotal = gm.getTotal();
+    }
+
+    public void update()
+    {
+        memoryAvailable = gm.getAvailable();
+        usage = (memoryAvailable/memoryTotal);
+    }
+
+    public long getMemoryAvailable() {
+        return memoryAvailable;
+    }
+
+    public long getMemoryTotal() {
+        return memoryTotal;
+    }
 }
