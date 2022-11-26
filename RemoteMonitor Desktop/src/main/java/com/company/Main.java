bbf7e2758772d6155e4569a0bdac2559ad7b72b3
omar <39051822+km@users.noreply.github.com>
2022-11-26 12:26:23 +0100

    Changes to Server class

--- RemoteMonitor Desktop/src/main/java/com/company/Main.java
+++ RemoteMonitor Desktop/src/main/java/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import components.ComponentManager;
+import networking.Server;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         ConfigurationManager configurationManager = new ConfigurationManager();
+        ComponentManager componentManager = new ComponentManager();
+        Server server = new Server(configurationManager.getPort(), configurationManager.getKeyword());
+
+        System.out.println("Connection Information");
+        System.out.println("Local connection IP: " + configurationManager.getLocalIp() + " Port: " + configurationManager.getPort());
+        System.out.println("Keyword: " + configurationManager.getKeyword());
+        server.start();
+
+        while (server.isConnected())
+        {
+            
+        }
+
+
+
+
 
     }
 }
--- RemoteMonitor Desktop/src/main/java/networking/Server.java
+++ RemoteMonitor Desktop/src/main/java/networking/Server.java
@@ -18,13 +18,23 @@ public class Server {
         connectionSocket = new ServerSocket(listeningPort);
     }
 
-    public void start() throws IOException
+    public boolean start() throws IOException
     {
         listener = connectionSocket.accept();
         connectedIp = listener.getRemoteSocketAddress().toString();
-        System.out.println(connectedIp);
         writer = new PrintWriter(listener.getOutputStream(), true);
         reader = new BufferedReader(new InputStreamReader(listener.getInputStream()));
+        //Will keep looping the function till a client connects with a correct keyword
+        if (!checkAuthed())
+        {
+            System.out.println(connectedIp + " tried connecting with a wrong keyword");
+            listener.close();
+            return start();
+        }
+        else
+        {
+            return true;
+        }
     }
 
     public Boolean write(String json) throws IOException {
@@ -44,13 +54,21 @@ public class Server {
     public Boolean checkAuthed() throws IOException
     {
         String input = reader.readLine();
-        if (input.equals(keyword)){
-            isAuthenticated = true;
+        if (input != null){
+            if (input.equals(keyword))
+            {
+              isAuthenticated = true;
+            }
         }
         return isAuthenticated;
     }
 
     public String getConnectedIp() {
         return connectedIp;
+    }
+
+    public Boolean isConnected()
+    {
+        return listener.isConnected();
     }
 }
